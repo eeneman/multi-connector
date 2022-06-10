f507e19a25d4bd8ace98d4bff433433bdd2fd97d
Hemanth <hemanth@tibura.com>
2022-06-10 10:57:56 +0530

    fidelix

--- config/plugins/fidelix-2.js
+++ config/plugins/fidelix-2.js
@@ -4,12 +4,6 @@
  */
 const _ = require('lodash');
 
-/**
- * Fidelix request compose and response parsing.
- */
-
-
-
 
 /**
  * Composes request arguments.
@@ -19,18 +13,11 @@ const _ = require('lodash');
  * @return {Object}
  */
 const template = async (config, template) => {
-   
+    console.log(template.authConfig.path)
+    template.authConfig.path.val = template.authConfig.path[0].val[0].processValue //parseFloat(template.authConfig.path.val)
+    template.authConfig.path = template.authConfig.path;
+    console.log(template.authConfig.path)
 
-    
-        // 2. Latest data.
-        if (template.authConfig.function.includes('getPointDataList')) {
-            // Multiple values per request.
-            template.authConfig.path = {'point_ids': template.authConfig.path.map((id) => {return {string: id};})};
-        } else {
-            // One value per request (getPointData).
-            template.authConfig.path = {'point_ids': template.authConfig.path.map((id) => {return {point_id: id};})};
-        }
-   // console.log(template)
     return template;
 };
 
@@ -42,66 +29,37 @@ const template = async (config, template) => {
  * @return {Object}
  */
 const response = async (config, response) => {
-    console.log(response)
     if (!_.isObject(response)) {
         return response;
     }
-    console.log(response)
     let result = [];
 
-   
+    // 1. One value per request (latest).
+    const single = 'setPointDataResult';
 
-    // 2. Multiple values per request (latest).
-    const array = 'getPointDataListResult';
-    const dataObject = 'PointData';
-    const valueKey = 'getPointDataResult';
 
- 
+
     try {
-        if (Object.hasOwnProperty.call(response, array)) {
-            /** 1. */
-            if (Object.hasOwnProperty.call(response[array], 'PointData')) {
-                if (Array.isArray(response[array][dataObject])) {
-                    for (let i = 0; i < response[array][dataObject].length; i++) {
-                        if (Object.hasOwnProperty.call(response[array][dataObject][i], 'Id')) {
-                            const data = {
-                                hardwareId: response[array][dataObject][i]['Id'],
-                                [valueKey]: response[array][dataObject][i]['Value'],
-                            };
-                            result.push(data);
-                        }
-                    }
-                }
-            }
+       if (Object.hasOwnProperty.call(response, single)) {
+        console.log(response)
+
+            const data = {
+                [single]: response['setPointDataResult'],
+                hardwareId: response.hardwareId['point_id'],
+            };
+            result.push(data);
         } else {
             result = response;
         }
-
         
+
         return result;
     } catch (e) {
         return result;
     }
 };
 
-/**
- * Translates response id to request id.
- *
- * @param {Object} config
- * @param {String/Object} id
- * @return {String/Object}
- */
-const id = async (config, id) => {
-    let translation;
-    try {
-        translation = config.parameters.ids.find(reqId => Object.values(reqId).includes(id));
-        const keys = Object.keys(translation);
-        translation = keys.length === 1 && keys[0] === 'id' ? id : translation;
-    } catch (err) {
-        return id;
-    }
-    return translation || id;
-};
+
 
 /**
  * Filters data by point ids.
@@ -111,16 +69,34 @@ const id = async (config, id) => {
  * @return {Object}
  */
 const output = async (config, output) => {
-    const ids = [];
-    try {
-        ids.push(...config.parameters.ids.map(entry => entry.id || entry).flat());
-        output[config.output.object][config.output.array] = output[config.output.object][config.output.array]
-            .filter(i => !!i).filter(d => ids.map(id => JSON.stringify(id))
-                .includes(JSON.stringify(d[config.output.id].id || d[config.output.id])));
-        return output;
-    } catch (err) {
-        return output;
-    }
+   
+    var arr = [];
+console.log(config.parameters.targetObjectpointId)
+    output.data.sensors.forEach(function (item) {
+
+        item.measurements.forEach((data) => {
+            console.log(data)
+            arr.push(
+                {
+                "measurements": [
+                    data
+                ],
+                "pointId":  config.parameters.targetObject.pointId
+            }
+            )
+        });
+    });
+   
+
+    const result = {
+        [config.output.context]: config.output.contextValue,
+        [config.output.object]: {
+            [config.output.array]: arr,
+        },
+    };
+
+    return result;
+    
 };
 
 /**
@@ -130,6 +106,6 @@ module.exports = {
     name: 'fidelix-2',
     template,
     response,
-    id,
+    
     output,
 };
